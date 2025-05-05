c634f517f210aae69538d46bc0aba1b1de301355
Brennan Lewellyn <brenlew27@gmail.com>
2025-05-05 18:55:16 -0500

    secured the vault

---
+++ index.js
@@ -0,0 +1,15 @@
+const code1 = 7+3
+
+const code2 = (82/2) - 1
+
+const code3 = 89%50
+
+const message = "The Vault has been secured. The combination is:"
+
+const codeA = code1+"-"+code2+"-"+code3
+
+const codeB = code1-code2-code3
+
+console.log(message)
+console.log(codeA)
+console.log(codeB)
